ed0d91171ff78bb58dea4c27a95cba360e60de55
germanavaz <gmsvaz@gmail.com>
2023-08-29 22:07:43 -0300

    atualização lista

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 const catalogo = [{
     id: 1,
-    marca: "Farm",
-    nome: "Roupa",
-    preco: 60,
-    imagem: "product-1.jpg",
-    feminino: true,
+    nome: "Adesivo Gatinhos",
+    preco: 7,
+    imagem: "product-1.png",
+    adesivo: true,
 }, 
 {
     id: 2,
-    marca: "Farm",
-    nome: "Roupa",
-    preco: 70,
-    imagem: "product-2.jpg",
-    feminino: true,
+    nome: "Adesivo Gatinho na Nave",
+    preco: 7,
+    imagem: "product-2.png",
+    adesivo: true,
+}, 
+{
+    id: 3,
+    nome: "Boton Astronauta",
+    preco: 10,
+    imagem: "product-3.png",
+    adesivo: false,
+}, 
+{
+    id: 4,
+    nome: "Boton Mar e Sol",
+    preco: 10,
+    imagem: "product-4.png",
+    adesivo: false,
 }
 ]
 
@@ -20,7 +32,6 @@ for (produto of catalogo) {
     const cartaoProduto = `
         <div id="card-produto">
             <img src="./assets/img/${produto.imagem}" alt="">
-            <p>${produto.marca}</p>
             <p>${produto.nome}</p>
             <p>$${produto.preco}</p>
             <button>Adicionar</button>
