57dd0c5135973170fc42dc343522d947e8628cd4
germanavaz <gmsvaz@gmail.com>
2023-08-30 23:40:07 -0300

    add card produto no carrinho

--- main.js
+++ main.js
@@ -48,17 +48,53 @@ const catalogo = [{
 }
 ]
 
-for (produto of catalogo) {
-    const cartaoProduto = `
-        <div id="card-produto-${produto.id}" class="card-produto">
-            <img class ="prod-img" src="./assets/img/${produto.imagem}" alt="">
-            <p class="prod-marca">${produto.marca} </p>
-            <p class="prod-nome">${produto.nome}</p>
-            <p class="prod-preco">R$${produto.preco}</p>
-            <button class="button"><i class="fa-solid fa-cart-plus"></i></button>
-        </div>
-    `
-    
-    document.getElementById("container-produto").innerHTML += cartaoProduto;
+
+// CARTÃO PRODUTO 
+
+function renderizarCatalogo() {
+    for (produto of catalogo) {
+        const cartaoProduto = `
+            <div id="card-produto-${produto.id}" class="card-produto">
+                <img class ="prod-img" src="./assets/img/${produto.imagem}" alt="">
+                <p class="prod-marca">${produto.marca} </p>
+                <p class="prod-nome">${produto.nome}</p>
+                <p class="prod-preco">R$${produto.preco}</p>
+                <button class="button-produto"><i class="fa-solid fa-cart-plus"></i></button>
+            </div>
+        `
+        
+        document.getElementById("container-produto").innerHTML += cartaoProduto;
+    }
+
+}
+
+renderizarCatalogo();
+
+// MENU CARRINHO 
+
+const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
+const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
+
+function toggleCarrinho() {
+    const carrinho = document.getElementById("carrinho");
+    carrinho.classList.toggle('active');
+
+}
+
+botaoAbrirCarrinho.addEventListener('click', toggleCarrinho);
+botaoFecharCarrinho.addEventListener('click', toggleCarrinho);
+
+function adicionarAoCarrinho() {
+    const containerProdutosCarrinho = document.getElementById("produtos-carrinho");
+    const cartaoProdutoCarrinho = ` <article>
+    <i class="fa-solid fa-xmark" style="color:black; position: absolute; top: 0; right: 0; padding: .2rem .2rem 0 0;" onMouseOver="this.style.color='#FF0000'" onMouseOut="this.style.color='#000'"></i>
+    <img src="./assets/img/product-1.png" alt="">
+    <div>
+        <p id="nome-produto-carrinho">Nome do produto</p>
+        <p>R$70</p>
+    </div>
+    </article>`;
+
+    containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
 
